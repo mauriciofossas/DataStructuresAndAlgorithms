bba277884915b658ad6267e64fa49ba436c2720a
mauriciofossaskoval <mauriciofossaskoval@mauricios-air.dhcp.wustl.edu>
2019-12-21 16:06:07 -0600

    Skyline tests

--- dataStructures/src/dataStructures/DataStructuresTests.java
+++ dataStructures/src/dataStructures/DataStructuresTests.java
@@ -24,5 +24,18 @@ class DataStructuresTests {
 		boolean result = ana.isAnagram("and", "dam");
 		assertEquals(false, result);
 	}
-	
+	@Test
+	void skyline1() {
+		keepCitySkyline skyline = new keepCitySkyline();
+		int[][] grid = {{3,0,8,4},{2,4,5,7},{9,2,6,3},{0,3,1,0}};
+		int result = skyline.maxIncreaseKeepingSkyline(grid);
+		assertEquals(35, result);
+	}
+	@Test
+	void skyline2() {
+		keepCitySkyline skyline = new keepCitySkyline();
+		int[][] grid = {{9,9,9},{9,9,9},{9,9,9}};
+		int result = skyline.maxIncreaseKeepingSkyline(grid);
+		assertEquals(0, result);
+	}
 }
